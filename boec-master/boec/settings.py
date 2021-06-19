bb7330f254dc29d545266a861e32cabb18441bb1
yasuo9x <ahvana0@gmail.com>
2021-06-19 15:04:49 +0700

    update

--- boec-master/boec/settings.py
+++ boec-master/boec/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 
     'boec_core',
     'boec_admin',
